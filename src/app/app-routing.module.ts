157e0b4838f30e5175fb561670d8df15712b3146
conorburke <cjburke89@gmail.com>
2020-08-08 20:44:42 +0900

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent} from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+    {
+        path: '',
+        children: [
+            { path: 'about', component: AboutComponent},
+            { path: 'contact', component: ContactComponent},
+            { path: 'services', component: ServicesComponent},
+        ]
+    },
+    // { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+    { path: '**', redirectTo: '', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
